50616333347145776fd0fa30c07faa144ce8d849
zhangjj <zhangjj@gw.com>
2014-06-30 11:36:34 +0800

    []解决u-boot编译不通过的问题

--- u-boot/ver_mk.sh
+++ u-boot/ver_mk.sh
@@ -1,5 +1,5 @@
 export CROSS_COMPILE=mips_4KC-
-svn_ver=`svn info|grep Rev:|awk -F' ' '{print $4}'`
-echo "version=$svn_ver"
+#svn_ver=`svn info|grep Rev:|awk -F' ' '{print $4}'`
+echo "version=1"
 make clean
-make BUILDNUMBER=$svn_ver DEFINES="-DBUILDNUMBER=$svn_ver -DCONFIG_LZMA"
+make BUILDNUMBER=1 DEFINES="-DBUILDNUMBER=1 -DCONFIG_LZMA"
