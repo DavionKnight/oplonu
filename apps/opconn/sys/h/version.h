c23c61782f79e61e63ed32705a03ba4831eec2e9
zhangjj <zhangjj@gw.com>
2014-11-24 15:33:21 +0800

    【版本发布】GT873_A软件 V1R01B111 2014-11-24 (Build time: 14:27:18, Nov 24 2014)

--- apps/opconn/sys/h/version.h
+++ apps/opconn/sys/h/version.h
@@ -31,10 +31,10 @@
  
 #define MAJOR_VERSION 1
 #define MINOR_VERSION 1
-#define BUILD_NUMBER 110
+#define BUILD_NUMBER 111
 #define MAJOR_VERSION_t "1"
 #define MINOR_VERSION_t "1"
-#define BUILD_NUMBER_t "110"
+#define BUILD_NUMBER_t "111"
 
 
 #if defined(ONU_1PORT_V1)
